43b186d4a9e995c67f650355f49f7064d7f8bfbb
Renata <141981631+FernandesRenata@users.noreply.github.com>
2023-10-06 10:46:33 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,54 +1,29 @@
 
-/* Esse projeto tem como objetivo acionar o led somente quado duas chaves estão acionadas*/
+/* Esse projeto tem como objetivo piscar o led*/
 
 
 #include "main.h"
 #include "stm32f4xx.h"
 
-int chaves;
 
-int main()
-{
-
-
+int main(void){
 
 	RCC->AHB1ENR=0x00000087;
-	GPIOC->MODER &=~(GPIO_MODER_MODER0|GPIO_MODER_MODER1|GPIO_MODER_MODER2|GPIO_MODER_MODER3);
-	GPIOB->MODER |= GPIO_MODER_MODER0_0;
+	GPIOC->MODER=GPIO_MODER_MODER0_0;
+	RCC->APB2ENR|=1<<18;
+
+	TIM11->PSC=1999;
+	TIM11->ARR=1599;
+	TIM11->CR1=0X01;
+	while(1){
+
+	if(TIM11->SR & TIM_SR_UIF){
 
 
-  while (1)
-  {
-
-	  chaves = GPIOC->IDR & 0b1111;
-
-	  switch(chaves){
-	  case 0b0110:
-		  GPIOB->ODR=0b01;
-		  	  break;
-	  case 0b1001:
-		  GPIOB->ODR=0b01;
-		  	  break;
-	  case 0b1100:
-		  GPIOB->ODR=0b01;
-		  	  break;
-	  case 0b0011:
-		  GPIOB->ODR=0b01;
-		  	  break;
-	  case 0b1010:
-		  GPIOB->ODR=0b01;
-		  	 break;
-	  case 0b0101:
-		  GPIOB->ODR=0b01;
-		    break;
+			GPIOC->ODR^=0b01;
+			TIM11->SR&=~(TIM_SR_UIF);
 
 
-	  default:
-		  GPIOB->ODR=0b00;
-		  break;
-
-
-	  }
-
-  }
+		}
+	}
 }
